b160213cb6e36596343836ca3d2fa2b71d0d6dc1
Anderson Encarnación Adames <adamesgithub@gmail.com>
2024-01-10 14:47:06 -0400

    p

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 export  { default as openapiToPostmanCollection } from "./lib/helpers/openapi-to-postman-collection";
 export  { default as docFileoperation } from "./lib/scripts/doc-file-operation";
 export  { getCollection,deleteCollection } from "./lib/scripts/doc-file-operation";
+/**
+ * import { docFileoperation } from "postman-collection-forge"
+
+const data = {
+    collection: {
+        info: {
+            name: "mane info",
+            schema: "info eschema example"
+        },
+        item: [
+            {
+                name: "Simple API definition",
+                request: "https://postman-echo.com/post"
+            }
+        ]
+    }
+}
+
+docFileoperation(data,"import","PMAK-6598263000ee803db2e098fc-bc9e24b5a156576a3996979810784e1809")
+ */
